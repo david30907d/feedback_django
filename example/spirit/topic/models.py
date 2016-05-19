d2d89c6fa666599db62d8da9bf336f411710bc42
david <davidtnfsh@gmail.com>
2016-05-19 14:13:19 +0800

    將course models從spirit資料夾一棟到get_feedback的app裏面

--- example/spirit/topic/models.py
+++ example/spirit/topic/models.py
@@ -100,25 +100,3 @@ class Topic(models.Model):
         Topic.objects\
             .filter(pk=self.pk)\
             .update(comment_count=F('comment_count') - 1)
-
-from datetime import datetime
-# Create your models here.
-class Course(models.Model):
-  school = models.CharField(max_length=10)
-  courseID = models.CharField(max_length=15)
-  name = models.CharField(max_length=15)
-  professor = models.CharField(max_length=10)
-  book = models.CharField(max_length=50)
-  # those five field is for feedback on spirit.
-  feedback_amount = models.DecimalField(max_digits=10,decimal_places=0,default=0)
-  feedback_freedom = models.FloatField()
-  feedback_FU = models.FloatField()
-  feedback_easy = models.FloatField()
-  feedback_GPA = models.FloatField()
-  feedback_knowledgeable = models.FloatField()
-  # those five field is for feedback on spirit.
-  create = models.DateTimeField()
-  def __str__(self):
-    return self.name
-  def check_courseID_and_name_not_empty(self):
-    return (self.courseID!=None and self.courseID!="") and (self.name!=None and self.name!="")
